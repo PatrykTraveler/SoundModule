e526b29159e62a4021eef75c318562a7767564ed
patryktraveler <patryktraveler@gmail.com>
2019-11-01 22:09:35 +0100

    Changed event strategy

--- android/src/main/java/com/reactlibrary/Simulator.java
+++ android/src/main/java/com/reactlibrary/Simulator.java
@@ -1,18 +1,39 @@
 package com.reactlibrary;
 
+import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.Callback;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
 
 import java.util.Random;
 
 public class Simulator {
-    Callback touchKey;
-    Callback releaseKey;
     int keyRange = 12;
     Thread simulationThread;
+    ReactApplicationContext reactApplicationContext;
 
-    public Simulator(Callback touchKey, Callback releaseKey) {
-        this.touchKey = touchKey;
-        this.releaseKey = releaseKey;
+    public Simulator(ReactApplicationContext reactApplicationContext) {
+        this.reactApplicationContext = reactApplicationContext;
+    }
+
+    public void sendEvent(String eventName, WritableMap params) {
+        this.reactApplicationContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+                .emit(eventName, params);
+    }
+
+    private void touchKey(int note){
+        WritableMap params = Arguments.createMap();
+        params.putInt("type", 1);
+        params.putInt("note", note);
+        sendEvent("KeyEvent", params);
+    }
+
+    private void releaseKey(int note){
+        WritableMap params = Arguments.createMap();
+        params.putInt("type", 0);
+        params.putInt("note", note);
+        sendEvent("KeyEvent", params);
     }
 
     public void runSimulation() {
@@ -22,13 +43,13 @@ public class Simulator {
             public void run() {
                 while (true) {
                     int note = random.nextInt() % keyRange + 60;
-                    touchKey.invoke(note);
+                    touchKey(note);
                     try {
                         Thread.sleep(100 * (random.nextInt() % 5 + 1));
                     } catch (InterruptedException e) {
                         Thread.currentThread().interrupt();
                     }
-                    releaseKey.invoke(note);
+                    releaseKey(note);
                 }
             }
         };
--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-import { NativeModules } from "react-native";
+import { NativeEventEmitter, NativeModules } from "react-native";
 
 const { SoundModule } = NativeModules;
 
-export default SoundModule;
+export default function registerForKeyEvents(touchKey, releaseKey) {
+  const eventEmitter = new NativeEventEmitter(SoundModule);
+  eventEmitter.addListener("KeyEvent", event => {
+    if (event.type == 1) {
+      touchKey(event.note);
+    }
+
+    if (event.type == 0) {
+      releaseKey(event.note);
+    }
+  });
+}
