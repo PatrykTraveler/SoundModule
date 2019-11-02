71ea1c2dd170d301f40bd8599fea731ee06b077a
patryktraveler <patryktraveler@gmail.com>
2019-11-02 21:04:57 +0100

    Added checker callback

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ import { NativeEventEmitter, NativeModules } from "react-native";
 
 const { SoundModule } = NativeModules;
 
-function registerForKeyEvents(touchKey, releaseKey, ref) {
+function registerForKeyEvents(touchKey, releaseKey, checkAccuracy) {
   const eventEmitter = new NativeEventEmitter(SoundModule);
   eventEmitter.addListener("KeyEvent", event => {
     if (event.type == 1) {
-      touchKey(event.note, ref);
+      touchKey(event.note);
     }
 
     if (event.type == 0) {
-      releaseKey(event.note, ref);
+      releaseKey(event.note);
     }
+    checkAccuracy(event.note, event.type);
   });
 }
 
