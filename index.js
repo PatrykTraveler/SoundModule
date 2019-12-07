edfd4ab62c56981f3ba0ea03de17dc19547ee921
patryktraveler <patryktraveler@gmail.com>
2019-12-07 18:18:11 +0100

    Changed type codes

--- index.js
+++ index.js
@@ -2,18 +2,17 @@ import { NativeEventEmitter, NativeModules } from "react-native";
 
 const { SoundModule } = NativeModules;
 
-function init(touchKey, releaseKey, checkAccuracy) {
+function init(touchKey, releaseKey) {
   SoundModule.connectMidiDevice();
   const eventEmitter = new NativeEventEmitter(SoundModule);
   eventEmitter.addListener("KeyEvent", event => {
-    if (event.type == 1) {
+    if (event.type == -112) {
       touchKey(event.note);
     }
 
-    if (event.type == 0) {
+    if (event.type == -128) {
       releaseKey(event.note);
     }
-    checkAccuracy(event.note, event.type);
   });
 }
 
