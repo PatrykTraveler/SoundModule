cfceaedb30e664af2b4ffcce46999b17d6e66b4e
patryktraveler <patryktraveler@gmail.com>
2019-12-16 22:30:23 +0100

    Extended functions

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 import { NativeEventEmitter, NativeModules } from "react-native";
 
 const { SoundModule } = NativeModules;
+const NOTE_ON = -112;
+const NOTE_OFF = -128;
 
 function setUpMidi(touchKey, releaseKey) {
   SoundModule.connectMidiDevice();
@@ -16,8 +18,12 @@ function setUpMidi(touchKey, releaseKey) {
   });
 }
 
-function playNote(note) {
-  SoundModule.playNote(note);
+function playNote(note, duration) {
+  if (duration !== undefined) {
+    SoundModule.playNote(note, duration);
+  } else {
+    SoundModule.playNote(note);
+  }
 }
 
 function stopNote(note) {
